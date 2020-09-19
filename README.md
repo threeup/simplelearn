f842465d43eddc23c140c50a2cf72980a8a7956e
Sheridan <threeup@gmail.com>
2020-09-19 01:35:24 -0700

    first js

---
+++ README.md
@@ -0,0 +1,9 @@
+# BabylonJS Example
+
+```
+git clone https://github.com/WhiteGrouse/BabylonJS-Example.git
+cd BabylonJS-Example
+npm install
+npm run build
+npm run start
+```
---
+++ app/main.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
+const path = require("path");
+const url = require("url");
+let mainWindow;
+function createWindow() {
+    var electronOptions = {
+        width: 800,
+        height: 480,
+        x: 0,
+        y: 0,
+        darkTheme: true,
+        //fullscreen: true,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+        backgroundColor: "#000000"
+    };
+    mainWindow = new electron_1.BrowserWindow(electronOptions);
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "../index.html"),
+        protocol: "file:",
+        slashes: true,
+    }));
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+}
+electron_1.app.on("ready", createWindow);
+electron_1.app.on("window-all-closed", () => {
+    electron_1.app.quit();
+});
+electron_1.app.on("activate", () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
